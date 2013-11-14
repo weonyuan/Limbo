76330635f51564587d9a19ca96a717c378283ea3
Jaimobean <Jaimobean@aol.com>
2013-11-14 14:18:11 -0500

    Password is now in dot form.
    Changed the password from text to password so now it appears as dots.

--- admin.php
+++ admin.php
@@ -44,7 +44,7 @@ if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
 <td>Username:</td><td><input type="text" name="username"></td>
 </tr>
 <tr>
-<td>Password:</td><td><input type="text" name="password"></td>
+<td>Password:</td><td><input type="password" name="password"></td>
 </tr>
 </table>
 <p><input type="submit" ></p>
