68ced72aa9e87b47ab9d9fca9120bc1d7180c2a1
Jaimobean <Jaimobean@aol.com>
2013-11-15 00:40:26 -0500

    found search
    don't know if this is okay but i copy and pasted your code from your
    lost search to make the found search. seems to work...

--- found.php
+++ found.php
@@ -5,9 +5,9 @@
   
   <body>
     <div>
-      <a href="lost.php">Lost something</a>&nbsp;
-      <a href="found.php">Found something</a>&nbsp;
-      <a href="admin.php">Admins</a>
+      <a href="lost.php">Lost something</a>;
+      <a href="found.php">Found something</a>;
+      <a href="admin.php">Admins</a>;
     </div>
     
     <p><a href="limbo.php"> Home</a> > Found Something</p>
@@ -17,17 +17,36 @@
          If you have found something, search for it here!
     </div>
     
-    <div>
-      <form action = "found_search_form.asp">
-        <input type="search"
-        name="foundsearch"> <input type = "submit">
+   <div>
+      <form action="found.php" method="POST">
+        <input type="search" name="item"> <input type="submit">
         Reported in last
         <select name="lastReported">
-          <option value="7 days">7 days</option>
+          <option value="7">week</option>
+          <option value="30">month</option>
+          <option value="90">3 months</option>
+          <option value="183">6 months</option>
+          <option value="365">year</option>
         </select>
       </form>
     </div>
-    
+	
+	<?php
+      # Connect to MySQL server and the database
+      require( 'includes/connect_limbo_db.php' ) ;
 
+      # Connect to MySQL server and the database
+      require( 'includes/found_tools.php' );
+      
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $item = $_POST['item'];
+        $reportedDate = $_POST['lastReported'];
+        //load('found.php', $item);
+        show_link_records($dbc, $item, $reportedDate);
+      }
+
+      # Close the connection
+      mysqli_close( $dbc );
+      ?>
   </body>
 </html>
