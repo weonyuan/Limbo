f1b5ce452ff2249c2ec7667e71b469d4fa4120f3
Jaimobean <Jaimobean@aol.com>
2013-11-16 01:57:29 -0500

    fixed form process so it has links

--- form_process.php
+++ form_process.php
@@ -1,26 +1,60 @@
-<?php
-
-//connect to limbo_db
-require( '/includes/connect_limbo_db.php' ) ;
-
-//select database
-mysqli_select_db ($dbc, 'limbo_db');
-
-//create variables for form
-$description = $_POST['description'];
-$room = $_POST['room'];
-$owner = $_POST['owner'];
-$finder = $_POST['finder'];
-$item_status = $_POST['item_status'];
-
-//insert data into table
-$sql = "INSERT INTO stuff (description, room, owner, finder, item_status) VALUES ('$description', '$room', '$owner', '$finder', '$item_status')";
-
-//error check
-if (!mysql_query($sql)) 
-	{
-	die('Error: ' . mysql_error());
-	}
+<html>
+  <head>
+    <title>Limbo - Added Ticked</title>
+  </head>
+  
+  <body>
+    <div>
+      <a href="lost.php">Lost something</a>
+      <a href="found.php">Found something</a>
+      <a href="admin.php">Admins</a>
+    </div>
 	
-mysql_close();
-?>
+	<p><a href="limbo.php"> Home</a> > <a href="admin.php"> Admin Login</a> > <a href="Admin-1.php"> Admin Options</a> > <a href="admin.php"> Add Ticket</a> > Ticket Added </p>
+	
+		<?php
+		#connect to limbo
+		define('DB_NAME', 'limbo_db');
+		define('DB_USER', 'root');
+		define('DB_PASSWORD', '');
+		define('DB_HOST', 'localhost');
+
+		$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+
+		#error check
+		if (!$link){
+			die('Could not connect: ' . mysql_error());
+			}
+
+		#select database
+		$db_selected = mysql_select_db(DB_NAME, $link);
+
+		if (!$db_selected) {
+			die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
+			}
+
+		#create variables for form
+		$description = $_POST['description'];
+		$room = $_POST['room'];
+		$owner = $_POST['owner'];
+		$finder = $_POST['finder'];
+		$item_status = $_POST['item_status'];
+		$location = $_POST['location'];
+
+
+		#insert data into table
+		$sql = "INSERT INTO stuff (location_id, description, create_date, update_date, room, owner, finder, item_status, ticket_status) VALUES ('$location', '$description', Now(), Now(), '$room', '$owner', '$finder', '$item_status', 'open')";
+
+
+
+		#error check
+		if (!mysql_query($sql)) 
+			{
+			die('Error: ' . mysql_error());
+			}
+		echo '<h1> Ticket Successfully Added </h1>';	
+		mysql_close();
+		?>
+
+	</body>
+</html>
