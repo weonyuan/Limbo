b102af94b0658e58e274d141b7a773c479c6fc3d
Jaimobean <Jaimobean@aol.com>
2013-11-15 21:55:38 -0500

    add ticket working now

--- addticket.php
+++ addticket.php
@@ -31,6 +31,42 @@
 		</p>
 		
 		<p>
+		Room:
+		<select name="location">
+			<option value="1">Byrne House</option>
+			<option value="2">James A. Cannavino Library</option>
+			<option value="3">Champagnat Hall</option>
+			<option value="4">Our Lady Seat Of Wisdom Chapel</option>
+			<option value="5">Cornell Boathouse</option>
+			<option value="6">Donnelly Hall</option>
+			<option value="7">Dyson Center</option>
+			<option value="8">Fern Tor</option>
+			<option value="9">Fontaine Hall</option>
+			<option value="10">Gartland Apartments</option>
+			<option value="11">Greystone Hall</option>
+			<option value="12">Kieran Gatehouse</option>
+			<option value="13">Kirk House</option>
+			<option value="14">Leo Hall</option>
+			<option value="15">Longview Park</option>
+			<option value="16">Lowell Thomas Communcations</option>
+			<option value="17">Lower Townhouses</option>
+			<option value="18">Marist Boathouse</option>
+			<option value="19">McCann Recreational Center</option>
+			<option value="20">Midrise Hall</option>
+			<option value="21">St. Ann's Hermitage</option>
+			<option value="22">St. Peter's</option>
+			<option value="23">Sheahan Hall</option>
+			<option value="24">Steel Plant Studios and Gallery</option>
+			<option value="25">Student</option>
+			<option value="26">Foy Townhouses</option>
+			<option value="27">Lower West Cedar Townhouses</option>
+			<option value="28">Upper West Cedar Townhouses</option>
+			<option value="29">Fulton Street Towenhouses</option>
+			<option value="30">Lower Fulton Townhouses</option>
+			<option value="31">Hancock Center</option>
+		</select>
+		
+		<p>
 		Owner (If Applies):
 		<input name="owner" type="text" />
 		</p>
--- form_process.php
+++ form_process.php
@@ -1,26 +1,43 @@
 <?php
+#connect to limbo
+define('DB_NAME', 'limbo_db');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
 
-//connect to limbo_db
-require( '/includes/connect_limbo_db.php' ) ;
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 
-//select database
-mysqli_select_db ($dbc, 'limbo_db');
+#error check
+if (!$link){
+    die('Could not connect: ' . mysql_error());
+	}
 
-//create variables for form
+#select database
+$db_selected = mysql_select_db(DB_NAME, $link);
+
+if (!$db_selected) {
+    die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
+	}
+
+#create variables for form
 $description = $_POST['description'];
 $room = $_POST['room'];
 $owner = $_POST['owner'];
 $finder = $_POST['finder'];
 $item_status = $_POST['item_status'];
+$location = $_POST['location'];
 
-//insert data into table
-$sql = "INSERT INTO stuff (description, room, owner, finder, item_status) VALUES ('$description', '$room', '$owner', '$finder', '$item_status')";
 
-//error check
+#insert data into table
+$sql = "INSERT INTO stuff (location_id, description, create_date, update_date, room, owner, finder, item_status, ticket_status) VALUES ('$location', '$description', Now(), Now(), '$room', '$owner', '$finder', '$item_status', 'open')";
+
+
+
+#error check
 if (!mysql_query($sql)) 
 	{
 	die('Error: ' . mysql_error());
 	}
-	
+echo 'Ticket Successfully Added.';	
 mysql_close();
 ?>
